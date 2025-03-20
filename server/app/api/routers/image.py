be4ef7955ecebfadb54cdb2925c8355ba27c48ba
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-03-20 19:44:07 -0300

    feat: sounds and threads

--- server/app/api/routers/image.py
+++ server/app/api/routers/image.py
@@ -6,6 +6,7 @@ from fastapi import (
 )
 from fastapi.responses import FileResponse, JSONResponse
 from threading import Thread
+from time import sleep
 
 
 from app.services.images import ImageServices
@@ -15,19 +16,47 @@ from app.utils.alert import play_notification_sound
 router = APIRouter(prefix="/images", tags=["Images"])
 
 activate_alert = True
+alert_thread = None
+
+def alert_loop():
+    while activate_alert:
+        play_notification_sound("app/data/bell.mp3")
+        sleep(1)  #
 
 @router.put("/toggle-sound")
 async def toggle_sound():
-    global activate_alert
+    global activate_alert, alert_thread
     activate_alert = not activate_alert
-    return {"detail": "Alert Activated" if activate_alert else "Alert Not Activated"}
+    if activate_alert and (alert_thread is None or not alert_thread.is_alive()):
+        alert_thread = Thread(target=alert_loop)
+        alert_thread.start()
+    return {
+        "detail": "Alert Activated" if activate_alert else "Alert Not Activated",
+        "activated": activate_alert
+    }
+
+@router.put("/deactivate-sound")
+async def toggle_sound():
+    global activate_alert, alert_thread
+    activate_alert = False
+    if activate_alert and (alert_thread is None or not alert_thread.is_alive()):
+        alert_thread = Thread(target=alert_loop)
+        alert_thread.start()
+    return {
+        "detail": "Alert Activated" if activate_alert else "Alert Not Activated",
+        "activated": activate_alert
+    }
+
 
 
 @router.post("")
 async def send_image(image: UploadFile):
     image_path = await ImageServices.save_image(image)
-    if activate_alert:
-        Thread(target=play_notification_sound, args=("app/data/bell.mp3",)).start()
+    global activate_alert, alert_thread
+    activate_alert = True
+    if activate_alert and (alert_thread is None or not alert_thread.is_alive()):
+        alert_thread = Thread(target=alert_loop)
+        alert_thread.start()
     return JSONResponse(
         content={"image_path": image_path},
         status_code=201
@@ -43,6 +72,12 @@ async def websocket_endpoint(websocket: WebSocket):
             image_path = await ImageServices.save_image(data)
             
             print(f"Image saved: {image_path}")
+            
+            global activate_alert, alert_thread
+            activate_alert = True
+            if activate_alert and (alert_thread is None or not alert_thread.is_alive()):
+                alert_thread = Thread(target=alert_loop)
+                alert_thread.start()
             
             await websocket.send_json({"image_path": image_path})
     except WebSocketDisconnect as e:
